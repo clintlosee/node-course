f829169860f20ccafb4f97c44221e3531cffb411
Clint Losee <clint.losee@gmail.com>
2015-12-21 08:12:19 -0700

    Add final sockets files

--- sockets/public/js/app.js
+++ sockets/public/js/app.js
@@ -1,13 +1,30 @@
 var socket = io();
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room');
+
+console.log(name + ' has joined ' + room);
+
+$('#room-name').text(room).rep;
 
 socket.on('connect', function() {
 	console.log('Connected to socket.io server!');
+	socket.emit('joinRoom', {
+		name: name,
+		room: room
+	});
 });
 
 socket.on('message', function(message) {
+	var momentTimestamp = moment.utc(message.timestamp);
+	var $messages = $('.messages');
+	var $message = $('<li class="list-group-item"></li>');
+
+
 	console.log('New Message: ' + message.text);
 
-	$('.messages').append('<p>' + message.text + '</p>');
+	$message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mma') + '</strong></p>')
+	$message.append('<p>' + message.text + '</p>');
+	$messages.append($message);
 });
 
 // Handle form submitting
@@ -19,8 +36,23 @@ $form.on('submit', function(e) {
 	var $message = $form.find('input[name=message]');
 
 	socket.emit('message', {
+		name: name,
 		text: $message.val()
 	});
 
 	$message.val('');
 });
+
+
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1].replace(/\+/g, ' '));
+        }
+    }
+    
+    return undefined;
+}
--- sockets/server.js
+++ sockets/server.js
@@ -3,19 +3,78 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var moment = require('moment');
 
 app.use(express.static(__dirname + '/public'));
+
+var clientInfo = {};
+
+// Send current users to socket
+function sendCurrentUsers(socket) {
+	var info = clientInfo[socket.id];
+	var users = [];
+
+	if (typeof info === 'undefined') {
+		return;
+	}
+
+	Object.keys(clientInfo).forEach(function(socketId) {
+		var userInfo = clientInfo[socketId];
+
+		if (info.room === userInfo.room) {
+			users.push(userInfo.name);
+		}
+	});
+
+	socket.emit('message', {
+		name: 'System',
+		text: 'Current Users: ' + users.join(', '),
+		timestamp: moment().valueOf()
+	});
+}
+
 
 io.on('connection', function(socket) {
 	console.log('User connected via socket.io!');
 
+	socket.on('disconnect', function() {
+		var userData = clientInfo[socket.id];
+
+		if (userData !== 'undefined') {
+			socket.leave(userData.room);
+			io.to(userData.room).emit('message', {
+				name: 'System',
+				text: userData.name + ' has left!',
+				timestamp: moment().valueOf()
+			});
+		}
+	});
+
+	socket.on('joinRoom', function(req) {
+		clientInfo[socket.id] = req;
+		socket.join(req.room);
+		socket.broadcast.to(req.room).emit('message', {
+			name: 'System',
+			text: req.name + ' has joined!',
+			timestamp: moment().valueOf()
+		});
+	});
+
 	socket.on('message', function(message) {
 		console.log('Message received: ' + message.text);
-		io.emit('message', message);
+
+		if (message.text === '@currentUsers') {
+			sendCurrentUsers(socket);
+		} else {
+			message.timestamp = moment().valueOf();
+			io.to(clientInfo[socket.id].room).emit('message', message);
+		}
 	});
 
 	socket.emit('message', {
-		text: 'Welcome to the chat application!'
+		name: 'System',
+		text: 'Welcome to the chat application!',
+		timestamp: moment().valueOf()
 	});
 });
 
