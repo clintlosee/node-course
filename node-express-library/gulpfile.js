fc4983cd805630174ca198b951ca702a9354049d
Clint Losee <clint.losee@gmail.com>
2016-04-13 08:17:48 -0600

    Adding templating engine to node-express-library (EJS) along with Jade test.

--- node-express-library/gulpfile.js
+++ node-express-library/gulpfile.js
@@ -30,7 +30,7 @@ gulp.task('inject', function() {
         ignorePath: '/public'
     };
 
-    return gulp.src('./src/views/*.html')
+    return gulp.src('./src/views/*.ejs')
         .pipe(wiredep(options))
         .pipe(inject(injectSrc, injectOptions))
         .pipe(gulp.dest('./src/views'));
@@ -41,7 +41,7 @@ gulp.task('serve', ['style', 'inject'], function() {
         script: 'app.js',
         delayTime: 1,
         env: {
-            'PORT': 3000
+            'PORT': 5000
         },
         watch: jsFiles
     };
