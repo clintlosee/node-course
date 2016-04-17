07fb1176e2f3c5fbf955ee2d0a70867524b2d9ce
Clint Losee <clint.losee@gmail.com>
2016-04-17 09:53:54 -0600

    Add findOne to node-express-library.

--- node-express-library/src/routes/bookRoutes.js
+++ node-express-library/src/routes/bookRoutes.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bookRouter = express.Router();
 var mongodb = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
 
 // Create router function and take passed in nav to use for links
 var router = function(nav) {
@@ -24,12 +25,18 @@ var router = function(nav) {
 
     bookRouter.route('/:id')
         .get(function(req, res) {
-        var id = req.params.id;
-        res.render('bookView', {
-            title: 'Books',
-            nav: nav,
-            book: books[id]
-        });
+          var id = new objectId(req.params.id);
+          var url = 'mongodb://localhost:27017/libraryApp';
+          mongodb.connect(url, function(err, db) {
+            var collection = db.collection('books');
+            collection.findOne({_id: id}, function(err, result) {
+              res.render('bookView', {
+                  title: 'Books',
+                  nav: nav,
+                  book: result
+                });
+            });
+          });
     });
 
     return bookRouter;
