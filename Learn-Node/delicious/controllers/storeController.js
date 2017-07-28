a8de9af9d162780e3d1ad2736449f5b1d2e191c5
Clint Losee <clint.losee@rcwilley.com>
2017-07-28 09:04:12 -0600

    Added geolocation and autocomplete for store data.

--- Learn-Node/delicious/controllers/storeController.js
+++ Learn-Node/delicious/controllers/storeController.js
@@ -31,6 +31,8 @@ exports.editStore = async (req, res) => {
 };
 
 exports.updateStore = async (req, res) => {
+    // set location data to be a point for map purposes
+    req.body.location.type = 'Point';
     // Find and update store
     const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
         new: true, // returns new store instead of old one
