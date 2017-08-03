7613c33668375e66bd2a608bf377ddcaa5f40920
Clint Losee <clint.losee@rcwilley.com>
2017-08-03 09:27:36 -0600

    Set up individual store routes and pages.

--- Learn-Node/delicious/controllers/storeController.js
+++ Learn-Node/delicious/controllers/storeController.js
@@ -75,3 +75,9 @@ exports.updateStore = async (req, res) => {
     req.flash('success', `Successfully updated ${store.name}. <strong><a href="/stores/${store.slug}">View Store</a></strong>`);
     res.redirect(`/stores/${store._id}/edit`);
 };
+
+exports.getStoreBySlug = async (req, res, next) => {
+    const store = await Store.findOne({ slug: req.params.slug });
+    if(!store) return next();
+    res.render('store', { title: `${store.name}`, store });
+};
