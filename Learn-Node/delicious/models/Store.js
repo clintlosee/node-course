56d35b2e6590d0df8227eb4cc87eb9a717a1addf
Clint Losee <clint.losee@rcwilley.com>
2017-08-03 09:36:02 -0600

    Set up for unique slugs on stores.

--- Learn-Node/delicious/models/Store.js
+++ Learn-Node/delicious/models/Store.js
@@ -23,10 +23,12 @@ const storeSchema = new mongoose.Schema({
             type: String,
             default: 'Point'
         },
-        coordinates: [{
-            type: Number,
-            required: 'You must supply coordinates!'
-        }],
+        coordinates: [
+            {
+                type: Number,
+                required: 'You must supply coordinates!'
+            }
+        ],
         address: {
             type: String,
             required: 'You must supply an address!'
@@ -35,15 +37,19 @@ const storeSchema = new mongoose.Schema({
     photo: String
 });
 
-storeSchema.pre('save', function(next) {
+storeSchema.pre('save', async function(next) {
     if (!this.isModified('name')) {
         next(); // skit it
         return; // stop function from running
     }
     this.slug = slug(this.name);
+    // find other stores with slug of same name or name-1, etc
+    const slugRegEx = new RegEx(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+    const storesWithSlug = await this.constructore.find({ slug: slugRegEx });
+    if (storesWithSlug.length) {
+        this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+    }
     next();
-
-    //TODO make so unique slugs exist
 });
 
 module.exports = mongoose.model('Store', storeSchema);
