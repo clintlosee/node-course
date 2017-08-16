9036cd25a18442db89a911d49087c066f85046b9
Clint Losee <clint.losee@rcwilley.com>
2017-08-16 08:11:41 -0600

    Set up user creation and login feature.

--- Learn-Node/delicious/routes/index.js
+++ Learn-Node/delicious/routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
 const { catchErrors } = require('../handlers/errorHandlers');
 
@@ -9,12 +11,14 @@ router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
 router.get('/add', storeController.addStore);
 
-router.post('/add', 
-    storeController.upload, 
-    catchErrors(storeController.resize), 
+router.post(
+    '/add',
+    storeController.upload,
+    catchErrors(storeController.resize),
     catchErrors(storeController.createStore)
 );
-router.post('/add/:id',
+router.post(
+    '/add/:id',
     storeController.upload,
     catchErrors(storeController.resize),
     catchErrors(storeController.updateStore)
@@ -25,5 +29,14 @@ router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 
 router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
+
+router.get('/login', userController.loginForm);
+router.get('/register', userController.registerForm);
+
+router.post('/register', 
+    userController.validateRegister,
+    userController.register,
+    authController.login
+);
 
 module.exports = router;
