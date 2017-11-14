4d94cf30f5c1c072f09c30ad461c9843094faff1
Clint Losee <clint.losee@rcwilley.com>
2017-11-14 08:22:36 -0700

    Set up pagination for main store page.

--- Learn-Node/delicious/controllers/storeController.js
+++ Learn-Node/delicious/controllers/storeController.js
@@ -51,9 +51,27 @@ exports.createStore = async (req, res) => {
 };
 
 exports.getStores = async (req, res) => {
+    const page = req.params.page || 1;
+    const limit = 6;
+    const skip = (page * limit) - limit;
+
     // query db for list of stores
-    const stores = await Store.find();
-    res.render('stores', { title: 'Stores', stores });
+    const storesPromise = Store
+        .find()
+        .skip(skip)
+        .limit(limit)
+        .sort({ created: 'desc' });
+    const countPromise = Store.count();
+
+    const [stores, count] = await Promise.all([storesPromise, countPromise]);
+    const pages = Math.ceil(count / limit);
+
+    if (!stores.length && skip) {
+        req.flash('info', `You asked for page ${page}, but that doesn't exist so I put you on page ${pages}`);
+        res.redirect(`/stores/page/${pages}`);
+        return;
+    }
+    res.render('stores', { title: 'Stores', stores, page, pages, count });
 };
 
 const confirmOwner = (store, user) => {
